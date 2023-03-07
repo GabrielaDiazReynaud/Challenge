2bb7ae6e6ddbe0b07aa67ca1333d13bea6391397
SupremeMaster23 <gabydiaz2000@hotmail.com>
2023-03-07 13:40:25 -0600

    Implememtacion de axios , llamada a la api para obtener todas las tarea

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
         <h1>Lista de Tarea</h1>
+        <h2>{{ taskList[0] }}</h2>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+import { ref, unref} from 'vue'
+
+export default{
+  setup() {
+  const taskList = ref([]);
+  taskList.value.push(1);
+
+  function getTasks(){
+    axios.get('http://127.0.0.1:8000/api/tasks').then(
+      response=>{
+        console.log(response);
+      }
+    );
+  }
+  getTasks();
+    return {
+      taskList
+    }
+  },
+
+}
+</script>
