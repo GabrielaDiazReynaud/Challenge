01ac85ab74495afcbc06d550845b211500798617
SupremeMaster23 <gabydiaz2000@hotmail.com>
2023-03-07 16:35:05 -0600

    Implementacion de checkbox y display de items con estilos

--- resources/js/Item.vue
+++ resources/js/Item.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        Item
+    <div class="item">
+        <input type="checkbox" @change="updateSelected()" v-model="taskIndv.selected "/>
+        <span :class="[taskIndv.selected ? 'done' :'textStyle']">{{taskIndv.task}}</span>
     </div>
 </template>
+<script>
+export default{
+    props:['taskIndv']
+}
+</script>
+<style scoped>
+.done{
+    text-decoration: line-through;
+    color:gray;
+}
+.textStyle{
+    width: 100%;
+    margin-left:20px;
+}
+.item{
+    display: flex;
+    justify-content:center;
+    align-items: center;
+}
+</style>
