98dabe15fb594ebc2906b9385b367782a687ab01
SupremeMaster23 <gabydiaz2000@hotmail.com>
2023-03-07 21:48:17 -0600

    Cambiao de css en el header

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,55 +1,63 @@
 <template>
     <div class="generalContainer">
-      <div class="headerTitleContainer">
-        <h1 id="title">Task List</h1>
-        <Form v-on:taskChange="getTasks()" :taskList="taskList"></Form>
-      </div>
-        <TaskListing v-on:reloadTasks="getTasks()" :taskList="taskList"></TaskListing>
- 
+      <h1 id="title">Lista De <span>Quehaceres</span></h1>
+        <div class="headerTitleContainer">
+            <Form v-on:taskChange="getTasks()" :taskList="taskList"></Form>
+        </div>
+        <TaskListing
+            v-on:reloadTasks="getTasks()"
+            :taskList="taskList"
+        ></TaskListing>
     </div>
 </template>
 
 <script>
-import Form from './Form.vue'
-import TaskListing from './TaskListing.vue'
-import axios from 'axios'
-import { ref, unref} from 'vue'
+import Form from "./Form.vue";
+import TaskListing from "./TaskListing.vue";
+import axios from "axios";
+import { ref, unref } from "vue";
 
-export default{
-  components:{
-    Form,
-    TaskListing
-  },
-  data:function(){
-    return{
-      taskList:[]
-    }
-  },
-  methods:{
-    getTasks(){
-    axios.get('http://127.0.0.1:8000/api/tasks').then(
-      response=>{
-        this.taskList= response.data;
-      }
-    );
-     },
-},
-created(){
-    this.getTasks();
-  }
-}
+export default {
+    components: {
+        Form,
+        TaskListing,
+    },
+    data: function () {
+        return {
+            taskList: [],
+        };
+    },
+    methods: {
+        getTasks() {
+            axios.get("http://127.0.0.1:8000/api/tasks").then((response) => {
+                this.taskList = response.data;
+            });
+        },
+    },
+    created() {
+        this.getTasks();
+    },
+};
 </script>
 
 <style scoped>
-.generalContainer{
-  width: 350px;
-  margin: auto;
+.generalContainer {
+    width: 350px;
+    margin: auto;
+    margin-top: 20px;
 }
-.headerTitleContainer{
-  background: rgb(200, 200, 200);
-  padding: 10px;
+.headerTitleContainer {
+    background: rgb(200, 200, 200);
+    padding: 10px;
 }
 #title {
-  text-align: center;
+    font-weight: 900;
+    font-size: 3rem;
+    line-height: 1;
+    text-align: center;
 }
+span{
+  color: #2563EB; 
+}
+
 </style>
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
         vue()
